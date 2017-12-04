585a7d9cae1a38ee1d2006f64361b8abd1446ff3
rogerluo410 <rogerluo410@gmail.com>
2017-12-04 15:57:12 +0800

    make it better

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,4 @@
-# CarrierWave.configure do |config|
+CarrierWave.configure do |config|
 #   # config.fog_provider = 'fog/aws'
 #   # config.fog_credentials = {
 #   #   provider:              'AWS',                                         # required
@@ -9,4 +9,4 @@
 #   # config.fog_directory = "#{ENV['AWSBucketName']}-#{Rails.env}"
 #   # config.fog_public     = false                                            # optional, defaults to true
 #   # config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
-# end
+end
