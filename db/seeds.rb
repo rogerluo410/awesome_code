fffc7c474e16ecca81cd4dd8fd2fd3182ae2da2e
rogerluo410 <rogerluo410@gmail.com>
2017-12-05 14:46:46 +0800

    Modify script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 if Rails.env.production?
   require 'factory_girl_rails'
+  require 'ffaker'
   # require Rails.root.join("db/seeds_basic").to_s
   require Rails.root.join("db/seeds_development").to_s
 
